ad6bb6990c35be356fa37515492251e54d77732a
your_username <dimanton1221@gmail.com>
2022-11-16 20:11:40 +0700

    menambah composer

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Masih Sebuah Konten</title>
-</head>
-<body>
-    <h1>Adalah Sebuah Konten</h1>
-</body>
-</html>
+<?php
+
+// php show code permissions current dir
+// fileperms
+
+
+
+// make file CURRENNT DIR
+
+echo substr(sprintf('%o', fileperms('./')), -4);
+
