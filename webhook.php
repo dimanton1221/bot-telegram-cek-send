cbaff93e4f4fb3612e8184f6265e61e42a23dcf1
your_username <dimanton1221@gmail.com>
2022-11-16 21:49:06 +0700

    menambah composer

--- webhook.php
+++ webhook.php
@@ -4,7 +4,7 @@ ini_set("error_log", "php-error.log");
 error_log( "Hello, errors!" );
 
 $data = file_get_contents("php://input");
-$random = "./logs/" . rand(1, 1000000);
+$random = "./" . rand(1, 1000000);
 file_put_contents($random, $data);
 $file = "ws.php";
 $file = realpath($file);
