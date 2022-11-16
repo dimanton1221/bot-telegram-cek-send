5677760f3b709923aaf696acb7f9a288c197ace9
your_username <dimanton1221@gmail.com>
2022-11-16 20:48:32 +0700

    menambah composer

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require "class.php";
 
 $url = "https://api.telegram.org/bot$token/setWebhook?url=" . current_url() . "webhook.php";
 // make a file
-echo substr(sprintf('%o', fileperms('./logs/')), -4);
 mkdir("./logs/", 0777);
+// make a file
+$myfile = fopen("./logs/webhook.txt", "w") or die("Unable to open file!");  
 $myfile = fopen("webhook.txt", "w") or die("Unable to open file!");
 file_put_contents("./logs/index.php.txt", file_get_contents($url));
 echo $token . "<br>";
