9a30b01e821595c0cedefef6926cf0aee043723d
your_username <dimanton1221@gmail.com>
2022-11-16 19:32:26 +0700

    percodingan

--- bot/index.php
+++ bot/index.php
@@ -5,7 +5,7 @@ require "class.php";
 
 
 $url = "https://api.telegram.org/bot$token/setWebhook?url=" . current_url() . "webhook.php";
-file_put_contents("logs/index.php.txt", file_get_contents($url));
+file_put_contents("./logs/index.php.txt", file_get_contents($url));
 echo $token . "<br>";
 
 echo "Server Sudah Berjalan :)";
