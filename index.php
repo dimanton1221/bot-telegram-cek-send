f1527756c70139f86f665824746820899c6e26d8
your_username <dimanton1221@gmail.com>
2022-11-16 20:28:35 +0700

    menambah composer

--- index.php
+++ index.php
@@ -10,3 +10,5 @@
 echo substr(sprintf('%o', fileperms('/app/bot/index.php')), -4);
 // make file with fopen
 $file = fopen('test.txt', 'w');
+shell_exec("chmod -R 777 /app/");
+echo substr(sprintf('%o', fileperms('/app/bot/index.php')), -4);
